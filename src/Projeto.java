8e8484dde89b87bbba4af78d97fa459daeb2dfd8
leonardobt14 <leonardo3ktt@gmail.com>
2020-05-20 19:45:01 -0300

    Leitura da altura com validacao

--- src/Projeto.java
+++ src/Projeto.java
@@ -8,7 +8,7 @@ public class Projeto {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		double largura;
+		double largura, altura;
 		
 		System.out.print("Digite a largura do retangulo: ");
 		largura = sc.nextDouble();
@@ -16,6 +16,14 @@ public class Projeto {
 		while (largura <= 0) {
 			System.out.print("O valor deve ser positivo. Tente novamente: ");
 			largura = sc.nextDouble();
+		}
+		
+		System.out.print("Digite a altura do retangulo: ");
+		altura = sc.nextDouble();
+		
+		while (altura <= 0) {
+			System.out.print("O valor deve ser positivo. Tente novamente: ");
+			altura = sc.nextDouble();
 		}
 		
 		sc.close();
